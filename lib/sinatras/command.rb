caae8d9cf5ce5ebfe1eff5b932f6e2f8eb0f7d44
tomcha <tomcha@tomcha-no-MacBook-Air.local>
2015-01-08 17:35:48 +0900

    commandアプリ実装

--- lib/sinatras/command.rb
+++ lib/sinatras/command.rb
@@ -7,6 +7,12 @@ module Sinatras
 
     def execute
       options = Options.parse!(@argv)
+      sub_command = options.delete(:command)
+
+      tasks = case sub_command
+              when 'new'
+                new
+              end
     end
 
     def self.run(argv)
@@ -17,5 +23,94 @@ module Sinatras
       @argv = argv
     end
 
+    def new
+      if optiond[:appname] !~ /^[a-z_]+[a-zA-Z0-9_]*$/
+        puts 'appname format is (a-z or _ ) + (alphabet,0-9, and _)'
+        exit
+      end
+    Dir.mkdir(appname, 0755)
+    puts "make directory ./" + appname + "/"
+    Dir.mkdir(appname + "/app", 0755)
+    puts "make directory ./" + appname + "/app/"
+    Dir.mkdir(appname + "/app/views", 0755)
+    puts "make directory ./" + appname + "/app/views/"
+    Dir.mkdir(appname + "/spec",  0755)
+    puts "make directory ./" + appname + "/spec/"
+    Dir.mkdir(appname + "/public", 0755)
+    puts "make directory ./" + appname + "/public/"
+    Dir.mkdir(appname + "/config", 0755)
+    puts "make directory ./" + appname + "/config/"
+      
+    File.open("./" + appname + "/Gemfile","w") do |file|
+      file.print <<__EOS__
+source "https://rubygems.org"
+gem 'sinatra'
+gem 'haml'
+gem 'rspec'
+__EOS__
+    end
+    puts "make file ./" + appname + "/Gemfile"
+
+    File.open("./" + appname + "/config.ru","w") do |file|
+      classname = appname.split("",2)
+      file.puts "require './app/" + appname + "'"
+      file.puts "run " + classname[0].upcase + classname[1]
+    end
+    puts "make file ./" + appname + "/config.ru"
+
+    File.open("./" + appname + "/app/" + appname + ".rb","w") do |file|
+      classname = appname.split("",2)
+      file.print <<__EOS__
+require 'sinatra/base'
+require 'haml'
+
+__EOS__
+      file.puts "class " + classname[0].upcase + classname[1] + "< Sinatra::Base"
+      file.print <<__EOS__
+  get '/' do
+    haml :index
+  end
+end
+__EOS__
+    end
+    puts "make file ./" + appname + "/app/" + appname + ".rb"
+ 
+    File.open("./" + appname + "/app/views/layout.haml","w") do |file|
+      file.print <<__EOS__
+!!!
+%html{lang: 'ja'}
+  %head
+    %meta{charset: 'utf-8'}
+    %title
+      hello,sinatras
+  %body
+    %div
+      =yield
+__EOS__
+    end
+    puts "make file ./" + appname + "/app/views/layout.haml"
+
+    File.open("./" + appname + "/app/views/index.haml","w") do |file|
+      file.print <<__EOS__
+%h1
+  Hello,Sinatras
+%p
+  powerd by Sinatra
+__EOS__
+    end
+    puts "make file ./" + appname + "/app/views/index.haml"
+    
+    File.chmod(0644, "./" + appname + "/Gemfile")
+    puts "change permission file ./" + appname + "/Gemfile"
+    File.chmod(0644, "./" + appname + "/config.ru")
+    puts "change permission file ./" + appname + "/config.ru"
+    File.chmod(0644, "./" + appname + "/app/" + appname+ ".rb")
+    puts "change permission file ./" + appname + "/app/" + appname + ".rb"
+    File.chmod(0644, "./" + appname + "/app/views/layout.haml")
+    puts "change permission file ./" + appname + "/app/views/layout.haml"
+    File.chmod(0644, "./" + appname + "/app/views/index.haml")
+    puts "change permission file ./" + appname + "/app/views/index.haml"
+    
+    end
   end
 end
